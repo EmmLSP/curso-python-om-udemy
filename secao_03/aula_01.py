0f581cc1660bc3119a3d075e000af96b01df56dc
EmmLSP <aexclusivasp@gmail..com>
2025-09-02 12:28:58 -0300

    corrigindo aula 01 secao 3

---
+++ secao_03/aula_01.py
@@ -0,0 +1,16 @@
+# comentario multilinhas DocStrings
+"""
+Docstrings nao e comentario e 
+o interpretador do python le
+docstrings
+"""
+
+'''
+doctrings com aspas simples
+'''
+
+# comentario usa somente uma linha
+
+# print() - funcao usada para exibir na tela
+print(123)
+print(456)
