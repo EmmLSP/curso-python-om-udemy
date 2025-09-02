2a1be3033704b1766cc61b342a35e88dcef9e919
EmmLSP <aexclusivasp@gmail..com>
2025-09-02 13:21:08 -0300

    secao 03 - aula 03

---
+++ secao_03/aula_03.py
@@ -0,0 +1,29 @@
+# Tipos de Dados - Strings
+
+"""
+Python = Linguagem de programação
+Tipo de tipagem = Dinâmica / Forte
+str -> string -> texto
+Strings são textos que estão dentro de aspas
+"""
+print(1234)
+
+#  Aspas simples
+print('Otavio Miranda')
+print(1, 'Otavio "Miranda"')
+
+# Aspas duplas
+print("Otavio Miranda")
+print(2, "Otavio 'Miranda'")
+
+# Escape -> pula "" e exibe ""
+print("Otavio \"Miranda\"")
+
+# r -> exibir caractere de escape com aspa
+print(r"Otavio \"Miranda\"")
+
+# Escape -> pula '' e exibe ''
+print('Otavio \'Miranda\'')
+
+# r -> exibir caractere de escape com aspa
+print(r'Otavio \'Miranda\'')
