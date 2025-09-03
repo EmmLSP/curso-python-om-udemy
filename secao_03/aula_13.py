815b84d056706690080ecbb4502b140b100d5ca7
EmmLSP <aexclusivasp@gmail..com>
2025-09-03 14:42:34 -0300

    secao 03 - aula 13

---
+++ secao_03/aula_13.py
@@ -0,0 +1,14 @@
+# Uma introdução às f-strings (formatação de strings)
+
+nome = 'Emmanuel'
+altura = 1.71
+peso = 132
+# formula -> IMC = peso / (altura²)
+imc = peso / altura ** 2
+linha_1 = f'{nome} tem {altura:.2f} de altura,'
+linha_2 = f'pesa {peso} quilos e seu IMC é'
+linha_3 = f'{imc:.2f}'
+print(linha_1)
+print(linha_2)
+print(linha_3)
+print(...) # Ellipsis
