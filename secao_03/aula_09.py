d1c476cbb4e7fe993c23da58ea88cf82ef04d497
EmmLSP <aexclusivasp@gmail..com>
2025-09-03 12:01:41 -0300

    secao 03 - aula 09

---
+++ secao_03/aula_09.py
@@ -0,0 +1,29 @@
+# Introducao aos operadores aritmeticos em Python (matematica)
+
+adicao = 10 + 10
+print('Adição:', adicao)
+
+subtracao = 10 - 5
+print('Subtração:', subtracao)
+
+multiplicacao = 10 * 10
+print('Multiplicação:', multiplicacao)
+
+divisao = 10 / 2.2 # sempre return float
+print('Divisão:', divisao)
+
+divisao_inteira = 10 // 2.2 # sempre cortas casas decimais
+print('Divisão inteira:', divisao_inteira)
+
+exponenciacao = 2 ** 10 # potenciação
+print('Exponenciação:', exponenciacao)
+
+modulo = 55 % 2 # resto da divisão
+print('Módulo:', modulo)
+
+# checar se um numero é divisivel pelo outro
+print(10 % 8 == 0)
+print(56 % 8 == 0)
+print('Checar se é par ou impar!')
+print(10, 10 % 2 == 0)
+print(15, 15 % 2 == 0)
