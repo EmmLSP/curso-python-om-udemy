1880090006d3024bc058f5c7ca8b2cbc42b004c2
EmmLSP <aexclusivasp@gmail..com>
2025-09-03 14:09:51 -0300

    secao 03 - aula11

---
+++ secao_03/aula_11.py
@@ -0,0 +1,12 @@
+# Precedência entre os operadores aritméticos
+
+# 1. (n + n) -> resolve parenteses de dentro para fora
+# 2. **
+# 3. * / // %
+# 4. + -
+conta_1 = 1 + 1 ** 5 + 5 # 1 + 1 + 5 == 7
+print(conta_1)
+conta_2 = (1 + 1) ** (5 + 5) # 2 ** 10 == 1024
+print(conta_2)
+conta_3 = (1 + int(0.5 + 0.5)) ** (5 + 5) # 2 ** 10 == 1024
+print(conta_3)
