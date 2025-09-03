0e48bc4201453d475d2f13413be49a2d63cd6918
EmmLSP <aexclusivasp@gmail..com>
2025-09-03 14:44:12 -0300

    secao 03 - aula 13

--- secao_03/aula_13.py
+++ secao_03/aula_13.py
@@ -3,8 +3,8 @@
 nome = 'Emmanuel'
 altura = 1.71
 peso = 132
-# formula -> IMC = peso / (altura²)
 imc = peso / altura ** 2
+"f-strings"
 linha_1 = f'{nome} tem {altura:.2f} de altura,'
 linha_2 = f'pesa {peso} quilos e seu IMC é'
 linha_3 = f'{imc:.2f}'
