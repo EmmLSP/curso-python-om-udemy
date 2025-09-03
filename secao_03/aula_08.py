f9bb1c00acaf41d1c459e37a50218d0f6fc06294
EmmLSP <aexclusivasp@gmail..com>
2025-09-03 11:34:34 -0300

    secao 03 - aula 08

---
+++ secao_03/aula_08.py
@@ -0,0 +1,14 @@
+# Exercicio com variaveis e tipos de dados
+
+nome = 'Luiz Otávio'
+sobrenome = 'Miranda'
+idade = 30
+ano_nascimento = 2025 - idade
+maior_idade = idade >= 18
+altura_metros = 1.80
+print('Nome:', nome)
+print('Sobrenome:', sobrenome)
+print('Idade:', idade)
+print('Ano de nascimento:', ano_nascimento)
+print('É maior de idade?', maior_idade)
+print('Altura em metros:', altura_metros)
