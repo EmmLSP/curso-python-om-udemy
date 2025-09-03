5de9208ed838255a94ae0b776f088e48ef2327bc
EmmLSP <aexclusivasp@gmail..com>
2025-09-03 11:19:31 -0300

    secao 03 - aula 07

---
+++ secao_03/aula_07.py
@@ -0,0 +1,23 @@
+# variaveis sao usadas para salvar algo na memoria do computador.
+# PEP8: inicie as variaveis com letras minusculas, pode usar
+# numeros e underline _.
+# O sinal de = é o operador de atribuição. Ele é usado para
+# atribuir um valor a um nome (variavel).
+# Uso: nome_variavel = expressao
+
+nome_completo = 'Luiz Otavio Miranda'
+soma_dois_mais_dois = 2 + 2 # expressao '2 + 2'
+int_um = int('1')
+print(int_um, type(int_um))
+print(nome_completo, soma_dois_mais_dois)
+
+nome = 'Luiz'
+idade = 30
+maior_de_idade = idade >= 18
+print('Nome:', nome, 'Idade:', idade)
+print('É maior?', maior_de_idade)
+nome = 'Otavio'
+idade = 17
+maior_de_idade = idade >= 18
+print('Nome:', nome, 'Idade:', idade)
+print('É maior?', maior_de_idade)
