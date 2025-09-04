e6fb99d700412a8abb32f7314cf47b78d4eab90d
EmmLSP <aexclusivasp@gmail..com>
2025-09-04 12:13:21 -0300

    secao 03 - aula 19

---
+++ secao_03/aula_19.py
@@ -0,0 +1,24 @@
+# Operadores relacionais (de comparação) em Python
+
+# Operadores de comparação (relacionais)
+# OP      Significado         Exemplo (True)
+# >       maior               2 > 1
+# >=      maior ou igual      2 >= 1
+# <       menor               1 < 2
+# <=      menor ou igual      2 <= 2
+# ==      igual               'a' == 'a'
+# !=      diferente           'a' != 'b'
+
+# carregar shell do python -> terminal: pwd -> ls -> cd ./secao_03/ -> python -i aula_19.py
+# todo arquivo .py é um modulo
+
+maior = 2 > 1
+maior_ou_igual = 2 >= 2
+menor = 1 < 2
+menor_ou_igual = 2 <= 2
+igual = 'a' == 'a'
+diferente = 'a' != 'b'
+
+print('Olha meu print aqui:')
+# ver com shell interativo do python
+# python -i aula_19.py
