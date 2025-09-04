81ea6c5e4c4c19790cb726c9a078cb188da33efb
EmmLSP <aexclusivasp@gmail..com>
2025-09-04 11:47:49 -0300

    secao 03 - aula 18

---
+++ secao_03/aula_18.py
@@ -0,0 +1,25 @@
+# O Debugger do VS Code e o interpretador do Python lendo os códigos
+
+# if / elif      / else
+# se / se nao se / se nao
+
+condicao1 = False
+condicao2 = False
+condicao3 = True
+condicao4 = True
+
+if condicao1: # se a condicao for verdadeira (True)
+    print('Codigo para condicao 1!')
+elif condicao2:
+    print('Codigo para condicao 2!')
+elif condicao3:
+    print('Codigo para condicao 3!')
+elif condicao4:
+    pass
+else:
+    print('Nenhuma condição foi satisfeita!') 
+
+if 10 == 10:
+    print('Outro if')
+
+print('Fora do if')
