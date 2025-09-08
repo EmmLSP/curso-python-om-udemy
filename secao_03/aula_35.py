42310fe57d2e3e4eacdb01a7ec520585bac4b7e8
EmmLSP <aexclusivasp@gmail..com>
2025-09-08 14:47:48 -0300

    secao 03 - aula 35

---
+++ secao_03/aula_35.py
@@ -0,0 +1,22 @@
+# while - Condição em detalhes
+
+'''
+Repetições
+while (enquanto)
+Executa uma acao enquanto uma condicao e verdadeira
+Loop infinito -> quando um codigo nao tem fim
+'''
+
+contador = 0
+while contador < 10:
+    print(contador)
+    contador += 1
+
+print('...')
+
+contador = 0
+while contador <= 10:
+    contador += 1
+    print(contador)
+
+print('...')
