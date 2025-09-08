08f38015e3a9b39df57c7760b18e03a649726788
EmmLSP <aexclusivasp@gmail..com>
2025-09-08 15:00:23 -0300

    secao 03 - aula 36

---
+++ secao_03/aula_36.py
@@ -0,0 +1,34 @@
+# Operadores de atribuição com operadores aritméticos
+
+'''
+Operadores de atribuição
+= += -= *= /= //= **= %=
+'''
+
+""" contador = 0
+
+while contador <= 10:
+    contador = contador + 1
+    print(contador)
+
+print('Acabou') """
+
+# soma
+contador = 5
+contador += 1
+print(contador)
+
+# concatenacao
+contador = '1'
+contador += '2'
+print(contador)
+
+# repeticao
+contador = 5
+contador *= '-='
+print(contador)
+
+# potenciacao
+contador = 2
+contador **= 10
+print(contador)
