14f1ddcc6abe32c1d73817ae1bfa8706ceead671
EmmLSP <aexclusivasp@gmail..com>
2025-09-14 11:50:33 -0300

    secao 03 - aula 49

---
+++ secao_03/aula_49.py
@@ -0,0 +1,14 @@
+# for in com tipo list
+
+"""
+for in com listas
+for funciona com iteraveis
+"""
+
+lista = ['Maria', 'Helena', 'Luiz']
+
+for letra in 'ABC':
+    print(letra)
+
+for nome in lista:
+    print(nome, type(nome))
