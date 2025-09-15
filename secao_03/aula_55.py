6d1110d0ce13303df57865e201ffbe505d927778
EmmLSP <aexclusivasp@gmail..com>
2025-09-15 13:48:31 -0300

    secao 03 - aula 55

---
+++ secao_03/aula_55.py
@@ -0,0 +1,17 @@
+# Imprecisão dos números de ponto flutuante + round e decimal.Decimal
+
+"""
+Imprecissao de ponto flutuante
+Double-precision floating-point format IEEE 754
+"""
+
+import decimal
+
+# Decimal() -> usado em numeros grandes
+# com muitas casas decimais
+numero_1 = decimal.Decimal('0.1')
+numero_2 = decimal.Decimal('0.7') 
+numero_3 = numero_1 + numero_2
+print(numero_3)
+print(f'{numero_3:.2f}')
+print(round(numero_3, 2))
