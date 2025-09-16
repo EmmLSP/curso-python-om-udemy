dd060bfd0b867dc56f43b23b4e17aec3833e41b3
EmmLSP <aexclusivasp@gmail..com>
2025-09-16 14:37:37 -0300

    secao 03 - aula 61

--- secao_03/aula_61.py
+++ secao_03/aula_61.py
@@ -53,7 +53,7 @@ cpf = '74682489070'
 nove_digitos = cpf[:9] # 0/8
 contador_regressivo_1 = 10
 
-resultado_digito_1 = digito_1 = resultado = 0
+resultado_digito_1 = digito_1 = 0
 for digito_1 in nove_digitos:
     resultado_digito_1 += contador_regressivo_1 * int(digito_1)
     contador_regressivo_1 -= 1
@@ -61,7 +61,7 @@ for digito_1 in nove_digitos:
 digito_1 = (resultado_digito_1 * 10) % 11
 
 if digito_1 > 9:
-    resultado = 0
+    digito_1 = 0
 else:
-    resultado = digito_1
-print(f'O primeiro digito do CPF é {resultado}')
+    digito_1 = digito_1
+print(f'O primeiro digito do CPF é {digito_1}')
