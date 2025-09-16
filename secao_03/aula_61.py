4d8b8c9c50eb4df23230e7609c2b59ebd477cdce
EmmLSP <aexclusivasp@gmail..com>
2025-09-16 14:39:51 -0300

    secao 03 - aula 61

--- secao_03/aula_61.py
+++ secao_03/aula_61.py
@@ -53,13 +53,11 @@ cpf = '74682489070'
 nove_digitos = cpf[:9] # 0/8
 contador_regressivo_1 = 10
 
-resultado_digito_1 = digito_1 = 0
+resultado_digito_1 = 0
 for digito_1 in nove_digitos:
     resultado_digito_1 += contador_regressivo_1 * int(digito_1)
     contador_regressivo_1 -= 1
-
 digito_1 = (resultado_digito_1 * 10) % 11
-
 if digito_1 > 9:
     digito_1 = 0
 else:
